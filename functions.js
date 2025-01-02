f52cd8641dd3aa6dcfa5c811e6a594543fb393fd
terridb <terrideboer99@gmail.com>
2025-01-02 17:11:46 +0100

    Opdracht 1 afgerond

--- functions.js
+++ functions.js
@@ -13,21 +13,44 @@ console.log(greeting);
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
+function getSchool() {
+  return "Novi Hogeschool"
+}
 
+const school = getSchool();
 
+console.log(school);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
+function compliment() {
+  return "Lekker bezig met die functies, Jan!"
+}
 
+const functionCompliment = compliment();
 
+console.log(functionCompliment);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
+function getGrades() {
+  return [8, 8.5, 6, 7]
+}
 
-
+const grades = getGrades();
+console.log(grades);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+function getDetails() {
+  return {
+    firstName: 'Jan',
+    lastName: 'Janssen'
+  }
+}
+
+const name = getDetails();
+console.log(name);
